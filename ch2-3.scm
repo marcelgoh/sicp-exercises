eaddb8d40a5d01da7edf14b18ac7b2802d81a95d
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-07-02 01:21:34 -0600

    Finished binary trees section

--- ch2-3.scm
+++ ch2-3.scm
@@ -1,5 +1,5 @@
 #| Solutions to selected exercises from Structure and Interpretation of Computer Programs
- | by Abelson and Sussman. Written by Marcel Goh. Last updated 1 July 2018.
+ | by Abelson and Sussman. Written by Marcel Goh. Last updated 2 July 2018.
  |#
 
 ; Exercise 2.54
@@ -78,7 +78,7 @@
         (else
           (error "unknown expression type: DERIV" exp))))
 
-; SETS Example
+; SETS AS LISTS Example
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((= x (car set)) true)
@@ -122,3 +122,81 @@
                        (cons x1 (union-set (cdr set1) set2)))
                       ((> x1 x2)
                        (cons x2 (union-set set1 (cdr set2)))))))))
+
+; SETS AS BINARY TREES Example
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+(define (t-element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (t-element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+; Exercise 2.63
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+#| a. The two procedures produce the same result for each of the three trees in Fig. 2.16.
+ | b. tree-list-1 is slower because it calls append which needs to cdr down the lists. 
+ |#
+
+; Exercise 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result
+                (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+#| a. partial-tree finds the approximate centre of the list, sets it as the entry, then recursively
+ |    calls partial-tree on the left side and right side on the centre to create the branches.
+ |    (5 (1 () (2 () ())) (9 (7 () ()) (11 () ())))
+ | b. The order is proportional to the size of the list so it's O(n). 
+ |#
+
+; Exercise 2.65
+(define (t-union-set set1 set2)
+  (list->tree (union-set (tree->list-2 set1) 
+                         (tree->list-2 set2))))
+(define (t-intersection-set set1 set2)
+  (list->tree (intersection-set (tree->list-2 set1) 
+                                (tree->list-2 set2))))

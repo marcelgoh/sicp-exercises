d92299e8db7077c4eff3db4e89a1f3aca57ff678
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-07-01 23:19:14 -0600

    Finished ordered and unordered lists

--- ch2-3.scm
+++ ch2-3.scm
@@ -1,5 +1,5 @@
 #| Solutions to selected exercises from Structure and Interpretation of Computer Programs
- | by Abelson and Sussman. Written by Marcel Goh. Last updated 24 June 2018.
+ | by Abelson and Sussman. Written by Marcel Goh. Last updated 1 July 2018.
  |#
 
 ; Exercise 2.54
@@ -46,7 +46,6 @@
       (caddr p)
       (cons '* (cddr p))))
 ; (End Exercise 2.57)
-
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
@@ -58,7 +57,6 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
-
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp) (if (same-variable? exp var) 1 0))
@@ -80,3 +78,47 @@
         (else
           (error "unknown expression type: DERIV" exp))))
 
+; SETS Example
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+       (let ((x1 (car set1)) (x2 (car set2)))
+         (cond ((= x1 x2)
+                (cons x1 (intersection-set (cdr set1)
+                                           (cdr set2))))
+               ((< x1 x2)
+                (intersection-set (cdr set1) set2))
+               ((< x2 x1)
+                (intersection-set set1 (cdr set2)))))))
+; Exercise 2.61
+(define (adjoin-set x set)
+  (if (or (null? set) (< x (car set)))
+      (cons x set)
+      (cons (car set) (adjoin-set x (cdr set)))))
+; Exercise 2.59
+(define (unordered-union-set set1 set2)
+  (if (null? set1) 
+      set2
+      (unordered-union-set (cdr set1) (adjoin-set (car set1) set2))))
+#| Exercise 2.60: element-of-set? would remain the same, adjoin-set would become a simple (cons x set),
+ |                intersection-set would remain the same, union-set would become (append set1 set2).
+ |                element-of-set? (and consequently intersection-set) might become slightly slower because the 
+ |                lists are larger but adjoin-set and union-set become much faster. This representation
+ |                might be useful if computation is precious but space is abundant. 
+ |#
+; Exercise 2.62
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((= x1 x2)
+                       (cons x1 (union-set (cdr set1) (cdr set2))))
+                      ((< x1 x2)
+                       (cons x1 (union-set (cdr set1) set2)))
+                      ((> x1 x2)
+                       (cons x2 (union-set set1 (cdr set2)))))))))

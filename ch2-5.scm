16711d037e71a926f4fa0621852ac3b2bd2360d0
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-02-08 20:57:51 -0500

    Finished Exercise 2.91

--- ch2-5.scm
+++ ch2-5.scm
@@ -8,6 +8,7 @@
   (install-scheme-number-package)
   (install-rational-package)
   (install-coercion-package)
+  (install-polynomial-package)
   'done)
 
 ; helper put/get operations for testing
@@ -89,6 +90,7 @@
               (drop dropped)
               x))
         x)))
+(define (neg x) (apply-generic 'neg x))                    ; Exercise 2.88
 (define (real-part z) (apply-generic 'real-part z))
 (define (imag-part z) (apply-generic 'imag-part z))
 (define (magnitude z) (apply-generic 'magnitude z))
@@ -222,6 +224,8 @@
        (lambda (x y) (tag (* x y))))
   (put 'div '(scheme-number scheme-number)
        (lambda (x y) (tag (/ x y))))
+  (put 'neg '(scheme-number)                               ; Exercise 2.88
+       (lambda (x) (tag (- x))))
   (put 'raise '(scheme-number)
        (lambda (x) (scheme-number->complex x)))
   (put 'project '(scheme-number)                           ; Exercise 2.85
@@ -285,4 +289,131 @@
        (lambda (x y) (rational-equal? x y)))
   (put '=zero? '(rational)                                 ; Exercise 2.80
        (lambda (x) (= (numer x) 0)))
+  'done)
+
+; provided code for 2.5.3
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+(define (install-polynomial-package)
+  ; polynomial representation
+  (define (make-poly variable term-list) (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable v2) (eq? v1 v2)))
+  ; terms and term lists
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list) (car term-list))
+  (define (rest-term term-list) (cdr term-list))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+  ; adding and multiplying
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+            (let ((t1 (first-term L1))
+                  (t2 (first-term L2)))
+              (cond ((> (order t1) (order t2))
+                     (adjoin-term t1 (add-terms (rest-terms L1) L2)))
+                    ((< (order t1) (order t2))
+                     (adjoin-term t2 (add-terms L1 (rest-terms L2))))
+                    (else
+                      (adjoin-term
+                        (make-term (order L1)
+                                   (add (coeff t1) (coeff t2)))
+                        (add-terms (rest-terms L1)
+                                   (rest-terms L2)))))))))
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+            (make-term (+ (order t1) (order t2))
+                       (mul (coeff t1) (coeff t2)))
+            (mul-term-by-all-terms t1 (rest-terms L))))))
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polynomials not in same variable: ADD-POLY"
+               (list p1 p2))))
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polynomials not in same variable: MUL-POLY"
+               (list p1 p2))))
+  (define (zero?-poly p)                                   ; Exercise 2.87
+    (let ((tlist (term-list p)))
+      (if (empty-termlist? terms)
+          #t
+          (and (=zero? (coeff (first-term terms)))
+               (zero-poly? (make-poly (variable p) (rest-terms terms)))))))
+  (define (negate-terms tlist)                             ; Exercise 2.88
+    (if (empty-termlist? tlist)
+        (the-empty-termlist)
+        (let ((first (first-term terms)))
+          (adjoin-term
+            (make-term (order first) (neg (coeff first)))
+            (negate-terms (rest-terms terms))))))
+  (define (negate-poly p)
+    (make-poly (variable p) (negate-terms (term-list p))))
+  (define (sub-poly p1 p2)
+    (add-poly p1 (negate-poly p2)))
+  (define (div-terms dend dsor)                            ; Exercise 2.91
+    (if (empty-termlist? dend)
+        (list (the-empty-termlist) (the-empty-termlist))
+        (let ((t1 (first-term dend))
+              (t2 (first-term dsor)))
+          (if (> (order dsor) (order dend))
+              (list (the-empty-termlist) dend)
+              (let ((new-c (div (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((new-term (make-term new-o new-c)))
+                  (let ((product (mul-terms dsor (list new-t))))
+                    (let ((difference (add-terms dend (negate-terms product))))
+                      (let ((rest-of-result (div-terms difference dsor)))
+                        (list (cons new-term (car rest-of-result))
+                              (cadr rest-of-result)))))))))))
+  (define (div-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (let ((answer (div-terms (term-list p1)
+                                 (term-list p2))))
+          (list (make-poly (variable p1) (car answer))
+                (make-poly (variable p2) (cadr answer))))
+        (error "Polynomials not over same variable: DIV-POLY"
+               (list p2 p2))))
+
+  ; interface to rest of system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  (put '=zero? '(polynomial)                               ; Exercise 2.87
+       (lambda (p) (zero-poly? p)))
+  (put '=neg '(polynomial)                                 ; Exercise 2.88
+       (lambda (p) (tag (negate-poly p))))
+  (put 'sub '(polynomial)
+       (lambda (p1 p2) (tag (sub-poly p1 p2))))
+  (put 'div '(polynomial)                                  ; Exercise 2.91
+       (lambda (p1 p2) (list (tag (car (div-poly p1 p2)))
+                             (tag (cadr (div-poly p1 p2))))))
   'done)
